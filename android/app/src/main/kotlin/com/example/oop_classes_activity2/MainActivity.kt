9011b599f09ac1d3cf0d019dbe672b989a3bcec2
joshuadevs <aguilonjoshua16@gmail.com>
2025-08-13 00:19:43 +0800

    new

---
+++ android/app/src/main/kotlin/com/example/oop_classes_activity2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.oop_classes_activity2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'Jherick.dart';
+import 'Joshua.dart';
+import 'Bary.dart';
+import 'Jack.dart';
+void main() {
+  var joshua = Joshua('Joshua','Culaba,Biliran',19,09464506397);
+  joshua.describe();
+
+  var jherick = Jherick('Jherick','Caray2x,Naval,Biliran',17,09456876564);
+  jherick.describe();
+
+  var bary = Bary('Bary','Biliran,Biliran',18,09476870964);
+  bary.describe();
+
+  var jack = Jack('Jack-Jack','Naval,Biliran',18,09673210964);
+  jack.describe();
+}
